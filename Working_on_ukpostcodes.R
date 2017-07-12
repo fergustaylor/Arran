e43ac020408f140a73ea5161a918473166c0b487
Fergus Taylor <fergustay@gmail.com>
2017-07-13 00:15:36 +0100

    got overlay to work with individual shapefile groups of coordinates

--- Working_on_ukpostcodes.R
+++
@@ -1,5 +0,0 @@
-## Import data
-allcoordinates <- read.csv("/alldata/ukpostcodes.csv")
-
-## Selecting rows
-Arrancoordinates <- allcoordinates[c(941622:941469),c("postcode","latitude","longitude")]
